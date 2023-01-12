c638fc3d91732fdb4dd45f545d6cc370304c649f
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-12 22:25:13 +0530

    added useRef hook in todo.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import Todo from './components/Todo';
-import TodoTasks from './components/TodoTasks';
 import {useSelector} from 'react-redux';
 function App() {
   const tasks = useSelector((state)=>state.todoReducer)
   return (
     <div className="App">
       <Todo />
-     
+      {console.log(taskss)}
     </div>
   );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,34 @@
-import React,{useState} from 'react'
+import React,{useRef} from 'react'
 import './todo.css'
-
+import { useDispatch } from 'react-redux'
+import { add_Todo } from './redux/actions/actions'
 const Todo = () => {
-    const [task,setTask] = useState([])
+    const actionDispatch = useDispatch()
+    let valueOfInput = useRef()
+    
+    
+    
     const formHandler =(e)=>{
         e.preventDefault()
-        alert(task)
+        
+        const date = new Date()
+        const data = {
+            id:date.getTime(),
+            payload:valueOfInput.current.value
+        }
+        
+        valueOfInput.current.value =''
+        actionDispatch(add_Todo(data))
+        
+       
     }
-    const inputFieldHandler=(e)=>{
-        setTask(e.target.value)
-    }
+    
   return (
     <div className='form__container'>
         <h1>TODO LIST</h1>
        <form onSubmit={(e)=>formHandler(e)}>
         <label htmlFor="text" className='form__label'>Enter a Task</label>
-        <input type="text" name="text" id="text" className='form__input' onChange={inputFieldHandler}/>
+        <input type="text" name="text" id="text" className='form__input'  ref={valueOfInput} />
         <input type="submit" value="submit" className='form__btn'/>
        </form>
     </div>
