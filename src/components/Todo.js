0b552ac92819d65ce9cd9d9f220cd10ae4708f24
Bharath siddoji <bharathsiddoji5@gmail.com>
2023-01-13 10:43:21 +0530

    made changes in todo file

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -14,21 +14,17 @@ const Todo = () => {
       console.log(filtered)
       deleteDispatch(delete_Todo(filtered))
     }
-    
     const formHandler =(e)=>{
-        e.preventDefault()
-        
-        const date = new Date()
-        const data = {
-            id:date.getTime(),
-            payload:valueOfInput.current.value
-        }
-        
-        valueOfInput.current.value =''
-        actionDispatch(add_Todo(data))
-        
-       
-    }
+      e.preventDefault()
+      const date = new Date()
+      const data = {
+          id:date.getTime(),
+          payload:valueOfInput.current.value
+      }
+      
+      valueOfInput.current.value =''
+      actionDispatch(add_Todo(data))
+  }
     
   return (
     <div className='form__container'>
@@ -38,8 +34,6 @@ const Todo = () => {
         <input type="text" name="text" id="text" className='form__input'  ref={valueOfInput} required/>
         <input type="submit" value="submit" className='form__btn'/>
        </form>
-
-     
       <div className='todo__task'>
         {tasks.map((each)=><div key={each.id} className="task__container">
             <p className="task">{each.payload}</p>
